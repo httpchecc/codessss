9c3b344fecd96268e669516db4593aec6c59e9b9
httpchecc <86075519+httpchecc@users.noreply.github.com>
2021-06-17 22:17:52 +0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,9 +1,10 @@
 local gr = 0
 local id = 0
-
+warn("got")
 
 script.Parent.Touched:Connect(function(hit)
 	if hit.Parent:FindFirstChild("Humanoid") == true then
+			print("doing")
 		local plr = hit.Parent
 		if plr:GetRankInGroup(gr) < id then
 			script.Parent.CanCollide = false
